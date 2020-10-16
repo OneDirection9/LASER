c9c9270a948f73274336f55f8231b86b38b484bb
han_hans <han_hans@apple.com>
2020-10-16 16:39:19 +0800

    binarize UNPC
    Signed-off-by: han_hans <han_hans@apple.com>

--- scripts/binarize_data.sh
+++ scripts/binarize_data.sh
@@ -9,10 +9,10 @@ BPE_VOCAB="${LASER}/models/93langs.fvocab"
 
 
 ProcessEuroparl() {
-  bpe="${LASER}/data/europarl/bpe"
+  bpe="${LASER}/data/europarl/bpe93"
   data_bin="${LASER}/data/europarl/binarized"
 
-  lang_pairs=( "en-es" "de-en" "de-es" "de-fr" "en-fr" "es-fr" )
+  lang_pairs=( "en-it" )
 
   for lang_pair in "${lang_pairs[@]}"; do
     src=$(echo "${lang_pair}" | cut -d'-' -f1)
@@ -26,4 +26,25 @@ ProcessEuroparl() {
   done
 }
 
+
+ProcessUNPC() {
+  bpe="${LASER}/data/unpc/bpe93"
+  data_bin="${LASER}/data/unpc/binarized"
+
+  lang_pairs=( "en-zh" )
+
+  for lang_pair in "${lang_pairs[@]}"; do
+    src=$(echo "${lang_pair}" | cut -d'-' -f1)
+    tgt=$(echo "${lang_pair}" | cut -d'-' -f2)
+    /bin/rm "${data_bin}/dict.${src}.txt" "${data_bin}/dict.${tgt}.txt"
+    fairseq-preprocess --source-lang "${src}" --target-lang "${tgt}" \
+        --trainpref "${bpe}/train.${src}-${tgt}" \
+        --joined-dictionary --tgtdict "${BPE_VOCAB}" \
+        --destdir "${data_bin}" \
+        --workers 20
+  done
+}
+
+
 ProcessEuroparl
+ProcessUNPC
