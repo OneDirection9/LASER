e786aeb8daed8893ddbc7cf0b850281b628c3285
Zhipeng Han <hanzhipeng9@gmail.com>
2021-03-04 14:48:08 +0800

    Add setup.py
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

---
+++ setup.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import, division, print_function
+
+from setuptools import find_packages, setup
+
+
+def get_readme():
+    with open("README.md", "r") as f:
+        content = f.read()
+    return content
+
+
+install_requires = [
+    "transliterate",
+    "jieba",
+    "torch==1.0",
+    "fairseq==0.8.0",
+]
+
+extras_require = {
+    "dev": [
+        "flake8==3.8.4",
+        "isort==5.6.4",
+        "black==20.8b1",
+        "flake8-bugbear",
+        "flake8-comprehensions",
+    ],
+}
+
+setup(
+    name="fairseq-laser",
+    version="0.0.1",
+    description="LASER build upon fairseq",
+    long_description=get_readme(),
+    keywords="computer vision",
+    packages=find_packages(include=("laser.*",)),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities",
+    ],
+    author="Zhipeng Han",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    install_requires=install_requires,
+    extras_require=extras_require,
+)
