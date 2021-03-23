58cf776d3f7eed20743c9b23f7d626471fd8d2de
Zhipeng Han <hanzhipeng9@gmail.com>
2021-03-23 10:01:46 +0000

    Fix val.sh using relative config path
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- scripts/val.sh
+++ scripts/val.sh
@@ -5,12 +5,16 @@ if [ -z "${LASER}" ]; then
   exit
 fi
 
-data_bin="${LASER}/data/val/binarized"
-fairseq-generate "${data_bin}" \
-    --task translation_laser \
-    --lang-pairs it-en,en-it,zh-en,en-zh \
-    --gen-subset train \
-    --path "${LASER}/checkpoints/laser_lstm/checkpoint_last.pt" \
-    --user-dir laser/ \
-    --beam 5 --batch-size 128 --remove-bpe | tee tmp.out
+oup_file="tmp.out"
+fairseq-generate \
+    "cfgs/laser.json" \
+    --user-dir laser \
+    --task laser \
+    --gen-subset valid \
+    --path "${LASER}/checkpoint_last.pt" \
+    --beam 5 --batch-size 128 --remove-bpe | tee ${oup_file}
 
+grep ^T "${oup_file}" | cut -f2- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.ref"
+grep ^H "${oup_file}" | cut -f3- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.sys"
+
+fairseq-score --sys "${oup_file}.sys" --ref "${oup_file}.ref"
