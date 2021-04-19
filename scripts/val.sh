95936f6f318ed10b5e44fb8c120c308162ded6c5
Zhipeng Han <hanzhipeng9@gmail.com>
2021-04-19 10:03:02 +0000

    add generate function
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- scripts/val.sh
+++ scripts/val.sh
@@ -5,57 +5,30 @@ if [ -z "${LASER}" ]; then
   exit
 fi
 
-path="${LASER}/checkpoints/laser_lstm/checkpoint30.pt"
+path="${LASER}/checkpoints/laser_lstm/checkpoint25.pt"
 
-oup_file="en-it.out"
-fairseq-generate \
-    "cfgs/en-it.json" \
-    --user-dir laser \
-    --task laser \
-    --gen-subset valid \
-    --path "${path}" \
-    --beam 5 --batch-size 128 --remove-bpe | tee ${oup_file}
+generate() {
+  cfg_file=$1
+  oup_file=$2
 
-grep ^T "${oup_file}" | cut -f2- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.ref"
-grep ^H "${oup_file}" | cut -f3- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.sys"
+  fairseq-generate \
+      "${cfg_file}" \
+      --user-dir laser \
+      --task laser \
+      --gen-subset valid \
+      --path "${path}" \
+      --beam 5 --batch-size 128 --remove-bpe | tee "${oup_file}"
 
-oup_file="it-en.out"
-fairseq-generate \
-    "cfgs/it-en.json" \
-    --user-dir laser \
-    --task laser \
-    --gen-subset valid \
-    --path "${path}" \
-    --beam 5 --batch-size 128 --remove-bpe | tee ${oup_file}
+  grep ^T "${oup_file}" | cut -f2- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.ref"
+  grep ^H "${oup_file}" | cut -f3- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.sys"
+}
 
-grep ^T "${oup_file}" | cut -f2- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.ref"
-grep ^H "${oup_file}" | cut -f3- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.sys"
+generate "cfgs/en-it.json" "output/en-it.out"
+generate "cfgs/it-en.json" "output/it-en.out"
+generate "cfgs/en-zh.json" "output/en-zh.out"
+generate "cfgs/zh-en.json" "output/zh-en.out"
 
-oup_file="en-zh.out"
-fairseq-generate \
-    "cfgs/en-zh.json" \
-    --user-dir laser \
-    --task laser \
-    --gen-subset valid \
-    --path "${path}" \
-    --beam 5 --batch-size 128 --remove-bpe | tee ${oup_file}
-
-grep ^T "${oup_file}" | cut -f2- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.ref"
-grep ^H "${oup_file}" | cut -f3- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.sys"
-
-oup_file="zh-en.out"
-fairseq-generate \
-    "cfgs/zh-en.json" \
-    --user-dir laser \
-    --task laser \
-    --gen-subset valid \
-    --path "${path}" \
-    --beam 5 --batch-size 128 --remove-bpe | tee ${oup_file}
-
-grep ^T "${oup_file}" | cut -f2- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.ref"
-grep ^H "${oup_file}" | cut -f3- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.sys"
-
-fairseq-score --sys "en-it.out.sys" --ref "en-it.out.ref"
-fairseq-score --sys "it-en.out.sys" --ref "it-en.out.ref"
-fairseq-score --sys "en-zh.out.sys" --ref "en-zh.out.ref"
-fairseq-score --sys "zh-en.out.sys" --ref "zh-en.out.ref"
+fairseq-score --sys "output/en-it.out.sys" --ref "output/en-it.out.ref"
+fairseq-score --sys "output/it-en.out.sys" --ref "output/it-en.out.ref"
+fairseq-score --sys "output/en-zh.out.sys" --ref "output/en-zh.out.ref"
+fairseq-score --sys "output/zh-en.out.sys" --ref "output/zh-en.out.ref"
